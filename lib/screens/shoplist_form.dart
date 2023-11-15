9c6e2afea86865694e1f9cb4de33baab8effaa1b
Muhammad Hanif <muhammadhanif2404@gmail.com>
2023-11-15 22:27:46 +0700

    Tutorial 8 Done

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/screens/menu.dart';
 
 class ShopFormPage extends StatefulWidget {
     const ShopFormPage({super.key});
@@ -14,6 +19,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
     String _description = "";
     @override
     Widget build(BuildContext context) {
+        final request = context.watch<CookieRequest>();
         return Scaffold(
         appBar: AppBar(
             title: const Center(
@@ -24,6 +30,7 @@ class _ShopFormPageState extends State<ShopFormPage> {
             backgroundColor: Colors.indigo,
             foregroundColor: Colors.white,
         ),
+        drawer: const LeftDrawer(),
         body: Form(
             key: _formKey,
             child: SingleChildScrollView(
@@ -111,37 +118,33 @@ class _ShopFormPageState extends State<ShopFormPage> {
                                 backgroundColor:
                                     MaterialStateProperty.all(Colors.indigo),
                             ),
-                                onPressed: () {
+                                onPressed: () async {
                                     if (_formKey.currentState!.validate()) {
-                                        showDialog(
-                                            context: context,
-                                            builder: (context) {
-                                                return AlertDialog(
-                                                    title: const Text('Produk berhasil tersimpan'),
-                                                    content: SingleChildScrollView(
-                                                        child: Column(
-                                                            crossAxisAlignment:
-                                                            CrossAxisAlignment.start,
-                                                            children: [
-                                                                Text('Nama: $_name'),
-                                                                Text('Harga: $_price'),
-                                                                Text('Deskripsi: $_description'),
-                                                            ],
-                                                        ),
-                                                    ),
-                                                    actions: [
-                                                        TextButton(
-                                                            child: const Text('OK'),
-                                                            onPressed: () {
-                                                                Navigator.pop(context);
-                                                            },
-                                                        ),
-                                                    ],
-                                                );
-                                            },
-                                        );
+                                        // Kirim ke Django dan tunggu respons
+                                        final response = await request.postJson(
+                                            "http://10.0.2.2:8000/create-flutter/",
+                                            jsonEncode(<String, String>{
+                                                'name': _name,
+                                                'price': _price.toString(),
+                                                'description': _description,
+                                            }));
+                                        if (response['status'] == 'success') {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(const SnackBar(
+                                                content: Text("Produk baru berhasil disimpan!"),
+                                            ));
+                                            Navigator.pushReplacement(
+                                                context,
+                                                MaterialPageRoute(builder: (context) => MyHomePage()),
+                                            );
+                                        } else {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(const SnackBar(
+                                                content:
+                                                Text("Terdapat kesalahan, silakan coba lagi."),
+                                            ));
+                                        }
                                     }
-                                    _formKey.currentState!.reset();
                                 },
                             child: const Text(
                                 "Save",
